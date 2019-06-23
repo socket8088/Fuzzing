b1636583e02cc8d6cf3eda68c66a9bae009effec
Xavi Beltran <xavibeltran08@gmail.com>
2019-06-23 19:45:21 +0200

    Create BF-HTTP.py

---
+++ BF-HTTP.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Author: Xavi Bel
+# Webpage: xavibel.com
+# Date: 23/06/2019
+# BooFuzz HTTP fuzzing template
+
+from boofuzz import *
+import time
+
+def main():
+    session = Session(
+	#sleep_time=10,
+        target=Target(
+            connection=SocketConnection("192.168.1.147", 7510, proto='tcp')
+        ),
+    )
+
+    s_initialize(name="Request")
+    with s_block("Request-Line"):
+	s_static("GET /topology/index.html HTTP/1.1\r\n")
+
+    with s_block("Host-Line"):
+	s_static("Host", name='host-string')
+	s_delim(":")
+	s_delim(" ")
+	s_string("192.168.1.147",  name='host-ip-address')
+	s_delim("\r\n")
+
+    with s_block("Other-headers"):
+	s_static("User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0\r\n")
+	s_static("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n")
+	s_static("Accept-Language: en-US,en;q=0.5\r\n")
+	s_static("Accept-Encoding: gzip, deflate\r\n")
+	s_static("Connection: keep-alive\r\n")
+
+    s_static("\r\n", "Request-CRLF")
+
+    session.connect(s_get("Request"))
+
+    session.fuzz()
+
+
+if __name__ == "__main__":
+	main()
